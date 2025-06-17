f0ca8820bd6a07f3520284a3e338243615df0adc
devuser09 <devuser09@refabs365.com>
2025-06-17 20:59:14 +0000

    Updated outputs.tf

--- Modules/VirtualNetwork/outputs.tf
+++ Modules/VirtualNetwork/outputs.tf
@@ -1,6 +1,9 @@
 output "vnet_name" {
   value = azurerm_virtual_network.this.name
 }
+output "vnet_id" {
+  value = azurerm_virtual_network.this.id
+}
 
 output "subnet_ids" {
   value = azurerm_subnet.subnets[*].id
