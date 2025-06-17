48c0fc85d6e976bccd5141a64cdbc84f0acd86b9
devuser09 <devuser09@refabs365.com>
2025-06-17 20:11:21 +0000

    Updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,12 @@
+
 output "resource_group_name" {
-  description = "The name of the created Resource Group"
-  value       = module.resource_group.rg_name
+  value = module.resource_group.rg_name
 }
 
 output "vnet1_name" {
-  description = "Name of the first Virtual Network"
-  value       = module.vnet1.vnet_name
+  value = module.vnet1.vnet_name
 }
 
 output "vnet2_name" {
-  description = "Name of the second Virtual Network"
-  value       = module.vnet2.vnet_name
+  value = module.vnet2.vnet_name
 }
