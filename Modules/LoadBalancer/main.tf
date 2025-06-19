e4e243185862714309912aa52bc7119a659bff60
devuser09 <devuser09@refabs365.com>
2025-06-19 16:08:51 +0000

    Updated main.tf

--- Modules/LoadBalancer/main.tf
+++ Modules/LoadBalancer/main.tf
@@ -16,6 +16,34 @@ resource "azurerm_lb" "this" {
     public_ip_address_id = azurerm_public_ip.this.id
   }
 }
+resource "azurerm_lb_backend_address_pool" "backend" {
+  name            = "${var.name}-backend-pool"
+  loadbalancer_id = azurerm_lb.this.id
+}
+
+# ✅ Add Health Probe (Optional but recommended)
+resource "azurerm_lb_probe" "http" {
+  name                = "${var.name}-http-probe"
+  protocol            = "Http"
+  port                = 80
+  request_path        = "/"
+  interval_in_seconds = 15
+  number_of_probes    = 2
+  loadbalancer_id     = azurerm_lb.this.id
+}
+
+# ✅ Add Load Balancer Rule (Assuming Ingress is exposed on NodePort)
+resource "azurerm_lb_rule" "http" {
+  name                           = "${var.name}-http-rule"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = var.backend_port  # default 30080 for AKS ingress
+  frontend_ip_configuration_name = "PublicFrontend"
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.backend.id
+  probe_id                       = azurerm_lb_probe.http.id
+  loadbalancer_id                = azurerm_lb.this.id
+}
+
 
 output "lb_public_ip" {
   value = azurerm_public_ip.this.ip_address
