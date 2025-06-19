64001b748006ed3f7278adf9b8a7c40b54b44b7c
devuser09 <devuser09@refabs365.com>
2025-06-19 09:27:06 +0000

    Updated main.tf

--- Modules/VirtualNetwork/main.tf
+++ Modules/VirtualNetwork/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_subnet" "subnets" {
   count                = length(var.subnet_names)
   name                 = var.subnet_names[count.index]
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = [var.subnet_prefixes[count.index]]
 }
 
