661928c77004dc72c1833d329fca488cc18f7e5a
devuser09 <devuser09@refabs365.com>
2025-06-19 10:04:56 +0000

    Updated main.tf

--- Modules/VirtualNetwork/main.tf
+++ Modules/VirtualNetwork/main.tf
@@ -6,10 +6,10 @@ resource "azurerm_virtual_network" "this" {
 }
 
 resource "azurerm_subnet" "subnets" {
-  count                = length(var.subnet_names)
-  name                 = var.subnet_names[count.index]
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = [var.subnet_prefixes[count.index]]
+  count                   = length(var.subnet_names)
+  name                    = var.subnet_names[count.index]
+  resource_group_name     = var.resource_group_name
+  virtual_network_name    = azurerm_virtual_network.this.name
+  address_prefixes        = [var.subnet_prefixes[count.index]]
 }
 
