318900aa61d4156296ac4b42d473af606911abd0
devuser09 <devuser09@refabs365.com>
2025-06-19 16:09:11 +0000

    Updated variables.tf

--- Modules/LoadBalancer/variables.tf
+++ Modules/LoadBalancer/variables.tf
@@ -1,3 +1,16 @@
-variable "name" {}
-variable "location" {}
-variable "resource_group_name" {}
+variable "aks_node_private_ips" {
+  description = "Private IPs of the AKS nodes for backend pool"
+  type        = list(string)
+  default     = []
+}
+
+variable "vnet_id" {
+  description = "VNet ID where the AKS nodes reside"
+  type        = string
+}
+
+variable "backend_port" {
+  description = "Port exposed by the AKS ingress NodePort"
+  type        = number
+  default     = 30080
+}
