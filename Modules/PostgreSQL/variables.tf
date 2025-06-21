2c549ccceec6a17c13c268ab00bfc204872525cc
devuser09 <devuser09@refabs365.com>
2025-06-21 07:45:28 +0000

    Updated variables.tf

--- Modules/PostgreSQL/variables.tf
+++ Modules/PostgreSQL/variables.tf
@@ -1,17 +1,30 @@
+// variable "postgresql_server_name" {}
+// variable "resource_group_name" {}
+// variable "location" {}
+// variable "postgres_version" {
+//   default = "13"
+// }
+// variable "admin_username" {}
+// variable "admin_password" {
+//   sensitive = true
+// }
+// variable "sku_name" {
+//   default = "GP_Gen5_2"
+// }
+// variable "storage_mb" {
+//   default = 32768
+// }
+// variable "database_name" {}
 variable "postgresql_server_name" {}
 variable "resource_group_name" {}
 variable "location" {}
-variable "postgres_version" {
-  default = "13"
-}
+variable "postgres_version" {}
 variable "admin_username" {}
-variable "admin_password" {
-  sensitive = true
-}
-variable "sku_name" {
-  default = "GP_Gen5_2"
-}
-variable "storage_mb" {
-  default = 32768
-}
+variable "admin_password" {}
+variable "sku_name" {}
+variable "storage_mb" {}
 variable "database_name" {}
+variable "subnet_id" {}
+variable "private_dns_zone_id" {}
+variable "zone" {}
+variable "key_vault_id" {}
