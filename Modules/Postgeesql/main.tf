5c23af062e9a105481bb60f9a545e24e8ed962d5
devuser09 <devuser09@refabs365.com>
2025-06-21 07:24:02 +0000

    Added main.tf

---
+++ Modules/Postgeesql/main.tf
@@ -0,0 +1,38 @@
+resource "azurerm_postgresql_flexible_server" "this" {
+  name                   = var.postgresql_server_name
+  resource_group_name    = var.resource_group_name
+  location               = var.location
+  version                = var.postgres_version
+  administrator_login    = var.admin_username
+  administrator_password = var.admin_password
+  sku_name               = var.sku_name
+  storage_mb             = var.storage_mb
+
+  high_availability {
+    mode = "Disabled"
+  }
+
+  backup {
+    backup_retention_days = 7
+    geo_redundant_backup  = "Disabled"
+  }
+
+  authentication {
+    password_auth_enabled         = true
+    active_directory_auth_enabled = false
+  }
+}
+
+resource "azurerm_postgresql_flexible_server_database" "this" {
+  name      = var.database_name
+  server_id = azurerm_postgresql_flexible_server.this.id
+  charset   = "UTF8"
+  collation = "en_US.utf8"
+}
+
+resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_all" {
+  name             = "AllowAll"
+  server_id        = azurerm_postgresql_flexible_server.this.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "255.255.255.255"
+}
