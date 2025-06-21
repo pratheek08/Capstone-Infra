182691b6f20b2a976aebf75984b2d61a98cf7bf9
devuser09 <devuser09@refabs365.com>
2025-06-21 07:35:35 +0000

    Updated variables.tf

--- Modules/PostgreSQL/variables.tf
+++ Modules/PostgreSQL/variables.tf
@@ -1,17 +1,17 @@
 variable "postgresql_server_name" {}
 variable "resource_group_name" {}
 variable "location" {}
+variable "postgres_version" {
+  default = "13"
+}
 variable "admin_username" {}
 variable "admin_password" {
   sensitive = true
 }
-variable "database_name" {}
 variable "sku_name" {
   default = "Standard_B1ms"
 }
 variable "storage_mb" {
   default = 32768
 }
-variable "postgres_version" {
-  default = "15"
-}
+variable "database_name" {}
