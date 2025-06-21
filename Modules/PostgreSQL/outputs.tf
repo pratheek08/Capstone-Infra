ec5afb210b63727f95e27392a06f976d10693f3f
devuser09 <devuser09@refabs365.com>
2025-06-21 07:45:46 +0000

    Updated outputs.tf

--- Modules/PostgreSQL/outputs.tf
+++ Modules/PostgreSQL/outputs.tf
@@ -1,6 +1,13 @@
-output "postgresql_fqdn" {
-  value = azurerm_postgresql_flexible_server.this.fqdn
-}
+// output "postgresql_fqdn" {
+//   value = azurerm_postgresql_flexible_server.this.fqdn
+// }
+// output "postgresql_server_id" {
+//   value = azurerm_postgresql_flexible_server.this.id
+// }
 output "postgresql_server_id" {
   value = azurerm_postgresql_flexible_server.this.id
+}
+
+output "postgresql_fqdn" {
+  value = azurerm_postgresql_flexible_server.this.fqdn
 }
