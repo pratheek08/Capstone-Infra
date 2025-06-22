918b557ae8514cb5e4c54bf101d831df9f650405
devuser17 <devuser17@refabs365.com>
2025-06-22 07:08:50 +0000

    Deleted Applicationgateway

--- Modules/Applicationgateway/outputs.tf
+++
@@ -1,5 +0,0 @@
-# Modules/Applicationgateway/outputs.tf
-
-output "public_ip" {
-  value = azurerm_public_ip.appgw_pip.ip_address
-}
--- Modules/Applicationgateway/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "name" {}
-variable "location" {}
-variable "resource_group_name" {}
-variable "subnet_id" {}
